f0527e18bddeb989580ab04d3b49262c567f7a67
unknown <farhana87shaik@gmail.com>
2026-09-22 03:01:56 +0530

    second commit

--- array/array.py
+++ array/array.py
@@ -1,1 +1,15 @@
-print("hello world")
+public class Multi_dimensionalarray
+{
+    public static void main(String[]args)
+    {
+        int a[][]={{1,2,3},{2,4,5},{4,5,6}};
+        for (int i=0;i<3;i++)
+        {
+            for(int j=0;j<3;j++)
+            {
+                System.out.println(a[i][j]);
+            }
+        }
+    
+    }
+}
